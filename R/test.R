c179a4d13b375292e26cd89bc3162bbb7d6d82e1
jirlong <jirlong@users.noreply.github.com>
2021-05-29 02:08:55 +0800

    update

---
+++ R/test.R
@@ -0,0 +1,222 @@
+#' edu_village_original
+#' @format data.frame
+#'tibble [7,760 × 51] (S3: spec_tbl_df/tbl_df/tbl/data.frame)
+#' $ statistic_yyy                              : chr [1:7760] "109" "109" "109" "109" ...
+#' $ district_code                              : chr [1:7760] "65000010001" "65000010002" "65000010003" "65000010004" ...
+#' $ countytown                                 : chr [1:7760] "新北市板橋區" "新北市板橋區" "新北市板橋區" "新北市板橋區" ...
+#' $ village                                    : chr [1:7760] "留侯里" "流芳里" "赤松里" "黃石里" ...
+#' $ edu_age_15up_total                         : chr [1:7760] "1455" "1290" "841" "1049" ...
+#' $ edu_doctor_graduated_m                     : chr [1:7760] "3" "8" "5" "4" ...
+#' $ edu_doctor_graduated_f                     : chr [1:7760] "1" "2" "3" "3" ...
+#' $ edu_doctor_ungraduated_m                   : chr [1:7760] "2" "2" "0" "3" ...
+#' $ edu_doctor_ungraduated_f                   : chr [1:7760] "2" "2" "0" "0" ...
+#' $ edu_master_graduated_m                     : chr [1:7760] "50" "54" "40" "32" ...
+#' $ edu_master_graduated_f                     : chr [1:7760] "36" "36" "43" "48" ...
+#' $ edu_master_ungraduated_m                   : chr [1:7760] "10" "10" "11" "6" ...
+#' $ edu_master_ungraduated_f                   : chr [1:7760] "4" "12" "10" "7" ...
+#' $ edu_university_graduated_m                 : chr [1:7760] "163" "149" "111" "129" ...
+#' $ edu_university_graduated_f                 : chr [1:7760] "186" "204" "143" "147" ...
+#' $ edu_university_ungraduated_m               : chr [1:7760] "55" "42" "32" "44" ...
+#' $ edu_university_ungraduated_f               : chr [1:7760] "44" "30" "22" "19" ...
+#' $ edu_juniorcollege_2ys_graduated_m          : chr [1:7760] "37" "37" "14" "28" ...
+#' $ edu_juniorcollege_2ys_graduated_f          : chr [1:7760] "33" "43" "26" "43" ...
+#' $ edu_juniorcollege_2ys_ungraduated_m        : chr [1:7760] "7" "3" "4" "3" ...
+#' $ edu_juniorcollege_2ys_ungraduated_f        : chr [1:7760] "2" "5" "4" "4" ...
+#' $ edu_juniorcollege_5ys_final2y_graduated_m  : chr [1:7760] "44" "46" "28" "30" ...
+#' $ edu_juniorcollege_5ys_final2y_graduated_f  : chr [1:7760] "36" "45" "27" "25" ...
+#' $ edu_juniorcollege_5ys_final2y_ungraduated_m: chr [1:7760] "0" "2" "1" "2" ...
+#' $ edu_juniorcollege_5ys_final2y_ungraduated_f: chr [1:7760] "0" "2" "1" "0" ...
+#' $ edu_senior_graduated_m                     : chr [1:7760] "71" "58" "30" "46" ...
+#' $ edu_senior_graduated_f                     : chr [1:7760] "57" "49" "23" "52" ...
+#' $ edu_senior_ungraduated_m                   : chr [1:7760] "19" "10" "7" "20" ...
+#' $ edu_senior_ungraduated_f                   : chr [1:7760] "21" "14" "11" "22" ...
+#' $ edu_seniorvocational_graduated_m           : chr [1:7760] "109" "72" "40" "71" ...
+#' $ edu_seniorvocational_graduated_f           : chr [1:7760] "140" "100" "64" "82" ...
+#' $ edu_seniorvocational_ungraduated_m         : chr [1:7760] "17" "14" "7" "4" ...
+#' $ edu_seniorvocational_ungraduated_f         : chr [1:7760] "15" "8" "6" "8" ...
+#' $ edu_juniorcollege_5ys_first3y_ungraduated_m: chr [1:7760] "4" "6" "2" "3" ...
+#' $ edu_juniorcollege_5ys_first3y_ungraduated_f: chr [1:7760] "5" "6" "4" "2" ...
+#' $ edu_junior_graduated_m                     : chr [1:7760] "42" "28" "27" "31" ...
+#' $ edu_junior_graduated_f                     : chr [1:7760] "80" "43" "26" "35" ...
+#' $ edu_junior_ungraduated_m                   : chr [1:7760] "5" "7" "4" "3" ...
+#' $ edu_junior_ungraduated_f                   : chr [1:7760] "5" "11" "8" "6" ...
+#' $ edu_juniorvocational_graduated_m           : chr [1:7760] "12" "19" "0" "1" ...
+#' $ edu_juniorvocational_graduated_f           : chr [1:7760] "37" "24" "3" "1" ...
+#' $ edu_juniorvocational_ungraduated_m         : chr [1:7760] "0" "0" "0" "0" ...
+#' $ edu_juniorvocational_ungraduated_f         : chr [1:7760] "0" "0" "0" "0" ...
+#' $ edu_primary_graduated_m                    : chr [1:7760] "26" "22" "14" "25" ...
+#' $ edu_primary_graduated_f                    : chr [1:7760] "64" "58" "32" "52" ...
+#' $ edu_primary_ungraduated_m                  : chr [1:7760] "2" "1" "0" "0" ...
+#' $ edu_primary_ungraduated_f                  : chr [1:7760] "4" "3" "4" "4" ...
+#' $ edu_selftaughtl_m                          : chr [1:7760] "0" "0" "0" "0" ...
+#' $ edu_selftaughtl_f                          : chr [1:7760] "2" "0" "0" "1" ...
+#' $ edu_illiterate_m                           : chr [1:7760] "0" "0" "1" "1" ...
+#' $ edu_illiterate_f                           : chr [1:7760] "3" "3" "3" "2" ...
+#' - attr(*, "spec")=
+#'  .. cols(
+#'  ..   statistic_yyy = col_character(),
+#'  ..   district_code = col_character(),
+#'  ..   site_id = col_character(),
+#'  ..   village = col_character(),
+#'  ..   edu_age_15up_total = col_character(),
+#'  ..   edu_doctor_graduated_m = col_character(),
+#'  ..   edu_doctor_graduated_f = col_character(),
+#'  ..   edu_doctor_ungraduated_m = col_character(),
+#'  ..   edu_doctor_ungraduated_f = col_character(),
+#'  ..   edu_master_graduated_m = col_character(),
+#'  ..   edu_master_graduated_f = col_character(),
+#'  ..   edu_master_ungraduated_m = col_character(),
+#'  ..   edu_master_ungraduated_f = col_character(),
+#'  ..   edu_university_graduated_m = col_character(),
+#'  ..   edu_university_graduated_f = col_character(),
+#'  ..   edu_university_ungraduated_m = col_character(),
+#'  ..   edu_university_ungraduated_f = col_character(),
+#'  ..   edu_juniorcollege_2ys_graduated_m = col_character(),
+#'  ..   edu_juniorcollege_2ys_graduated_f = col_character(),
+#'  ..   edu_juniorcollege_2ys_ungraduated_m = col_character(),
+#'  ..   edu_juniorcollege_2ys_ungraduated_f = col_character(),
+#'  ..   edu_juniorcollege_5ys_final2y_graduated_m = col_character(),
+#'  ..   edu_juniorcollege_5ys_final2y_graduated_f = col_character(),
+#'  ..   edu_juniorcollege_5ys_final2y_ungraduated_m = col_character(),
+#'  ..   edu_juniorcollege_5ys_final2y_ungraduated_f = col_character(),
+#'  ..   edu_senior_graduated_m = col_character(),
+#'  ..   edu_senior_graduated_f = col_character(),
+#'  ..   edu_senior_ungraduated_m = col_character(),
+#'  ..   edu_senior_ungraduated_f = col_character(),
+#'  ..   edu_seniorvocational_graduated_m = col_character(),
+#'  ..   edu_seniorvocational_graduated_f = col_character(),
+#'  ..   edu_seniorvocational_ungraduated_m = col_character(),
+#'  ..   edu_seniorvocational_ungraduated_f = col_character(),
+#'  ..   edu_juniorcollege_5ys_first3y_ungraduated_m = col_character(),
+#'  ..   edu_juniorcollege_5ys_first3y_ungraduated_f = col_character(),
+#'  ..   edu_junior_graduated_m = col_character(),
+#'  ..   edu_junior_graduated_f = col_character(),
+#'  ..   edu_junior_ungraduated_m = col_character(),
+#'  ..   edu_junior_ungraduated_f = col_character(),
+#'  ..   edu_juniorvocational_graduated_m = col_character(),
+#'  ..   edu_juniorvocational_graduated_f = col_character(),
+#'  ..   edu_juniorvocational_ungraduated_m = col_character(),
+#'  ..   edu_juniorvocational_ungraduated_f = col_character(),
+#'  ..   edu_primary_graduated_m = col_character(),
+#'  ..   edu_primary_graduated_f = col_character(),
+#'  ..   edu_primary_ungraduated_m = col_character(),
+#'  ..   edu_primary_ungraduated_f = col_character(),
+#'  ..   edu_selftaughtl_m = col_character(),
+#'  ..   edu_selftaughtl_f = col_character(),
+#'  ..   edu_illiterate_m = col_character(),
+#'  ..   edu_illiterate_f = col_character()
+#'  .. )
+'edu_village_original'
+#' edu_town
+#' @format data.frame
+#'tibble [368 × 6] (S3: rowwise_df/tbl_df/tbl/data.frame)
+#' $ countytown           : chr [1:368] "高雄市阿蓮區" "高雄市大寮區" "高雄市大社區" "高雄市大樹區" ...
+#' $ population_15up      : num [1:368] 25235 100888 31081 37801 313656 ...
+#' $ university           : num [1:368] 6985 27653 9435 10162 112741 ...
+#' $ university.not       : num [1:368] 18250 73235 21646 27639 200915 ...
+#' $ university.female    : num [1:368] 3363 13039 4381 4900 54478 ...
+#' $ university.not.female: num [1:368] 9120 37201 11195 13280 113946 ...
+#' - attr(*, "groups")= tibble [368 × 1] (S3: tbl_df/tbl/data.frame)
+#'  ..$ .rows: list<int> [1:368] 
+#'  .. ..$ : int 1
+#'  .. ..$ : int 2
+#'  .. ..$ : int 3
+#'  .. ..$ : int 4
+#'  .. ..$ : int 5
+#'  .. ..$ : int 6
+#'  .. ..$ : int 7
+#'  .. ..$ : int 8
+#'  .. ..$ : int 9
+#'  .. ..$ : int 10
+#'  .. ..$ : int 11
+#'  .. ..$ : int 12
+#'  .. ..$ : int 13
+#'  .. ..$ : int 14
+#'  .. ..$ : int 15
+#'  .. ..$ : int 16
+#'  .. ..$ : int 17
+#'  .. ..$ : int 18
+#'  .. ..$ : int 19
+#'  .. ..$ : int 20
+#'  .. ..$ : int 21
+#'  .. ..$ : int 22
+#'  .. ..$ : int 23
+#'  .. ..$ : int 24
+#'  .. ..$ : int 25
+#'  .. ..$ : int 26
+#'  .. ..$ : int 27
+#'  .. ..$ : int 28
+#'  .. ..$ : int 29
+#'  .. ..$ : int 30
+#'  .. ..$ : int 31
+#'  .. ..$ : int 32
+#'  .. ..$ : int 33
+#'  .. ..$ : int 34
+#'  .. ..$ : int 35
+#'  .. ..$ : int 36
+#'  .. ..$ : int 37
+#'  .. ..$ : int 38
+#'  .. ..$ : int 39
+#'  .. ..$ : int 40
+#'  .. ..$ : int 41
+#'  .. ..$ : int 42
+#'  .. ..$ : int 43
+#'  .. ..$ : int 44
+#'  .. ..$ : int 45
+#'  .. ..$ : int 46
+#'  .. ..$ : int 47
+#'  .. ..$ : int 48
+#'  .. ..$ : int 49
+#'  .. ..$ : int 50
+#'  .. ..$ : int 51
+#'  .. ..$ : int 52
+#'  .. ..$ : int 53
+#'  .. ..$ : int 54
+#'  .. ..$ : int 55
+#'  .. ..$ : int 56
+#'  .. ..$ : int 57
+#'  .. ..$ : int 58
+#'  .. ..$ : int 59
+#'  .. ..$ : int 60
+#'  .. ..$ : int 61
+#'  .. ..$ : int 62
+#'  .. ..$ : int 63
+#'  .. ..$ : int 64
+#'  .. ..$ : int 65
+#'  .. ..$ : int 66
+#'  .. ..$ : int 67
+#'  .. ..$ : int 68
+#'  .. ..$ : int 69
+#'  .. ..$ : int 70
+#'  .. ..$ : int 71
+#'  .. ..$ : int 72
+#'  .. ..$ : int 73
+#'  .. ..$ : int 74
+#'  .. ..$ : int 75
+#'  .. ..$ : int 76
+#'  .. ..$ : int 77
+#'  .. ..$ : int 78
+#'  .. ..$ : int 79
+#'  .. ..$ : int 80
+#'  .. ..$ : int 81
+#'  .. ..$ : int 82
+#'  .. ..$ : int 83
+#'  .. ..$ : int 84
+#'  .. ..$ : int 85
+#'  .. ..$ : int 86
+#'  .. ..$ : int 87
+#'  .. ..$ : int 88
+#'  .. ..$ : int 89
+#'  .. ..$ : int 90
+#'  .. ..$ : int 91
+#'  .. ..$ : int 92
+#'  .. ..$ : int 93
+#'  .. ..$ : int 94
+#'  .. ..$ : int 95
+#'  .. ..$ : int 96
+#'  .. ..$ : int 97
+#'  .. ..$ : int 98
+#'  .. ..$ : int 99
+#'  .. .. [list output truncated]
+#'  .. ..@ ptype: int(0) 
+'edu_town'
